b3b31081b9ede98e33ba470c41c1188254cf6363
Haider <haider.haider168@gmail.com>
2018-08-09 01:28:33 +0530

    fixed: potential security vulnerabilities in dependencies

--- src/app/modules/ng-simple-autocomplete/components/ng-simple-autocomplete/ng-simple-autocomplete.component.ts
+++ src/app/modules/ng-simple-autocomplete/components/ng-simple-autocomplete/ng-simple-autocomplete.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output,EventEmitter, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
@@ -10,9 +10,9 @@ export class NgSimpleAutocompleteComponent implements OnInit {
 
   @Input() placeholder = '';
   @Input() sourceArray = [];
-  @Input() fieldNameFormatter:(value: any) => string;
+  @Input() fieldNameFormatter: (value: any) => string;
   @Output() onSelect: EventEmitter<any> = new EventEmitter();
-  @Input() multiSelect: boolean = false; 
+  @Input() multiSelect: boolean = false;
   @ViewChild('itemList') itemList: ElementRef;
   @ViewChild('inputBox') inputBox: ElementRef;
   selectedItemIndex = -1;
@@ -30,12 +30,12 @@ export class NgSimpleAutocompleteComponent implements OnInit {
       this.showDropDown = true;
       this.selectedItemIndex = -1;
     } else {
-        this.showDropDown = false;
+      this.showDropDown = false;
     }
   }
   selectValue(item) {
-  
-    if(this.multiSelect){
+
+    if (this.multiSelect) {
       this.mutliSelectedArray.push(item);
       this.onSelect.emit(this.mutliSelectedArray);
       this.searchText = '';
@@ -45,45 +45,48 @@ export class NgSimpleAutocompleteComponent implements OnInit {
       this.onSelect.emit(item);
       this.selectedItemIndex = -1;
     }
-    
+
   }
   selectItemByKeyboard(keyCode) {
     if (keyCode === 40) {
       // down key pressed
-      const numberOfFilteredElements = this.itemList.nativeElement.children.length -1;
+      const numberOfFilteredElements = this.itemList.nativeElement.children.length - 1;
       if (this.selectedItemIndex < numberOfFilteredElements) {
         this.selectedItemIndex++;
       }
-      
-    } else if (keyCode === 38){
+
+    } else if (keyCode === 38) {
       // up key pressed
       if (this.selectedItemIndex > 0) {
         this.selectedItemIndex--;
       }
     } else if (keyCode === 13) {
       // enter key pressed
-      const selectdValue:string = this.itemList.nativeElement.children[this.selectedItemIndex].innerHTML;
-      for (let i=0; i<this.sourceArray.length; i++) {
-        const value: string = this.fieldNameFormatter(this.sourceArray[i]);
-        if (selectdValue.trim() === value.trim()){
-          this.selectValue(this.sourceArray[i]);
-          this.inputBox.nativeElement.blur();
-          break;
+      const element = this.itemList.nativeElement.children[this.selectedItemIndex];
+      if (element !== undefined) {
+        const selectdValue: string = element.innerHTML
+        for (let i = 0; i < this.sourceArray.length; i++) {
+          const value: string = this.fieldNameFormatter(this.sourceArray[i]);
+          if (selectdValue.trim() === value.trim()) {
+            this.selectValue(this.sourceArray[i]);
+            this.inputBox.nativeElement.blur();
+            break;
+          }
         }
       }
     }
   }
-  updateSelectedItemIndex(index){
+  updateSelectedItemIndex(index) {
     this.selectedItemIndex = index;
   }
   onInputChange(val) {
-    this.selectedItemIndex  = -1;
-  } 
+    this.selectedItemIndex = -1;
+  }
 
-  focusInput(){
+  focusInput() {
     this.inputBox.nativeElement.focus();
   }
-  removeFromSeletedArray(index){
+  removeFromSeletedArray(index) {
     this.mutliSelectedArray.splice(index, 1);
   }
 }
